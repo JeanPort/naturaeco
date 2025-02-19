d1ea8bda1ea147f86db60f6a158745e80029658e
jeanport <pierreanatc522@gmail.com>
2025-02-19 16:15:40 -0300

    Iniciando projeto

---
+++ src/main/java/com/jean/naturaeco/NaturaecoApplication.java
@@ -0,0 +1,13 @@
+package com.jean.naturaeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturaecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturaecoApplication.class, args);
+	}
+
+}
